45c13b7f11edde15213b3430cbe4fdbfbe07c224
LeHarter <36102345+LeHarter@users.noreply.github.com>
2021-06-27 10:43:27 +0200

    Update Semantic_Learner.py

--- Semantic_Learner.py
+++ Semantic_Learner.py
@@ -1,59 +1,6 @@
 # The following code was written by Christopher Potts and Percy Liang. Their original file
 # is called synthesis.py. We deleted evaluate_interpretive and evaluate_latent_semparse and
-# adjusted evaluate_semparse and leaves  as written in the comments in these functions.
-
-#!/usr/bin/env python
-
-"""
-The intellectual core of the paper. For demos:
-
-python synthesis.py
-
-Our goal with this code is to illustrate how the learning framework
-from `learning.py` and the grammar framework from `grammar.py` come
-together.  
-
-`phi_sem` is the simple feature function used in figure 2 of the paper.
-We use it here for all of the illustrations.
-
-`crude_lexicon` is a grammar that respects typing but nothing else.
-Together with rules and functions from `grammar.py`, it defines a crude
-grammar. The goal of learning is to refine this grammar by finding the
-best pairings of lexical items and logical expressions.
-
-For all of the illustrations, the train/test data are created in
-`semdata` using a gold grammar, which has the same rules as the crude
-grammar and the same functions for interpretation, but its lexicon is
-perfect.
-
-`evaluate_semparse` runs the semantic parsing model of section 4.1 of
-the paper. Its training data predictions are perfect. It makes a
-predictable mistake on the test data: since it never sees 'four'/4
-in training, it gets the final test example wrong.
-
-`evaluate_interpretive` implements section 4.2 of the paper, using
-`LatentSGD` instead of `SGD` (both in `learning.py`). The role of the
-grammar is the same as in `evaluate_semparse`, and the resulting
-performance is the same as well.
-
-`evaluate_latent_semparse` goes beyond what is in the paper, to
-achieve more of a connection with published semantic parsing models.
-Here, we see only the root node of the logical form in training, so
-that the tree structure is a latent variable. To make it interesting,
-we add a type-lifting rule to the grammar, so that many final logical
-forms correspond to multiple distinct derivations. LatentSGD is used
-for optimization, and the resulting performance is the same as for
-the other models.
-
-"""
-
-
-__author__ = "Christopher Potts and Percy Liang"
-__credits__ = []
-__license__ = "GNU general public license, version 2"
-__version__ = "2.0"
-__maintainer__ = "Christopher Potts"
-__email__ = "See the authors' websites"
+# adjusted evaluate_semparse and leaves as written in the comments in these functions.
 
 
 import re
@@ -78,29 +25,9 @@ def phi_sem(x, y):
     for leaf in y.components:
         d[leaf] += 1.0
     return d
-    """
-    for leaf in leaves(y):
-        d[leaf] += 1.0
-    return d"""
-
-"""def leaves(x):
-    #Recursive function for finding all the preterminals (mother--child)
-    #trees. Used by phi_sem
-    # Leaf-only trees:
-    if len(x[1])==1 and isinstance(x[1][0],str):
-        return [(x[0],x[1][0])] # Adjusted so it fit our backpointer implementation we build in the CYK-parser
-    # Recursive call for all child subtrees:
-    l = []
-    for child in x[1]: # Adjusted so it fit to our backpointer implementation we build in the CYK-parser
-        l += leaves(child)
-    return l"""
 
 
-def evaluate_semparse(u,lfs,grammar,allparses): # We give evaluate_semparse an utterance, an lf and a grammar as arguments so wen can use it for our interactive game
-    """Evaluate the semantic parsing set-up, where we learn from and 
-    predict logical forms. The set-up is identical to the simple 
-    example in evenodd.py, except that classes is gram.gen, which 
-    creates the set of licit parses according to the crude grammar."""    
+def evaluate_semparse(u,lfs,grammar,allparses): # We give evaluate_semparse an utterance, an lf and a grammar as arguments so wen can use it for our interactive game  
     print("======================================================================")
     print("SEMANTIC PARSING")
     # Only (input, lf) pairs for this task:
